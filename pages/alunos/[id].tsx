d8b716cb0727bddc3e7a92658c8dbaad554e442e
JordanoBaluz <jordanolb95@hotmail.com>
2022-09-14 22:11:50 -0300

    getStaticPaths para gerar os paths que serão compilados para gerar os paths em paginas estaticas

--- pages/alunos/[id].tsx
+++ pages/alunos/[id].tsx
@@ -1,24 +1,40 @@
-export function getStaticPaths() {
+export async function getStaticPaths() {
+    const resp = await fetch('http://localhost:3000/api/alunos/tutores')
+
+    const ids = await resp.json()
+
+    const paths = ids.map(id => {
+        return { params: { id: `${id}` } }
+    })
+
     return {
         fallback: false, // se não encontrar retorna 404
-        paths: [
-            { params: { id: '1' } },
-            { params: { id: '2' } },
-            { params: { id: '3' } },
-        ]
+        paths
     }
 }
 
-export function getStaticProps() {
+export async function getStaticProps(context) {
+    const resp = await fetch(`http://localhost:3000/api/alunos/${context.params.id}`)
+
+    const aluno = await resp.json()
+
     return {
-        props: {}
+        props: {
+            aluno
+        }
     }
 }
 
-export default function AlunoPorId() {
+export default function AlunoPorId(props) {
+    const { aluno } = props
     return (
         <div>
             <h1>Detalhes dos alunos</h1>
+            <ul>
+                <li>{aluno.id}</li>
+                <li>{aluno.nome}</li>
+                <li>{aluno.email}</li>
+            </ul>
 
         </div>
     )
